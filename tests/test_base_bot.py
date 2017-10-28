686229e5e7e51100c5c42edc7c6dbd616ece1f01
Nikita Kharlov <nkharlov@ptsecurity.com>
2017-10-28 18:57:01 +0300

    chat-object tests

--- tests/test_base_bot.py
+++ tests/test_base_bot.py
@@ -34,3 +34,36 @@ def test_telegram_object_getattr_proxy(telegram_object):
 def test_telegram_object_getattr_no_exist(telegram_object):
     with pytest.raises(KeyError):
         telegram_object.test_not_exist_property
+
+
+def test_chat_object_factory():
+    from english_cards.base_bot import Chat
+    chat_first_create = Chat({
+        'id': 'test_chat_id'
+    })
+
+    chat_second_create = Chat({
+        'id': 'test_chat_id'
+    })
+
+    chat_other = Chat({
+        'id': 'other_test_chat_id'
+    })
+
+    assert chat_first_create is chat_second_create
+    assert chat_second_create is not chat_other
+
+
+def test_chat_object_storage():
+    from english_cards.base_bot import Chat
+    chat_first_create = Chat({
+        'id': 'test_chat_id'
+    })
+
+    chat_first_create.storage['test'] = 'test_value'
+
+    chat_second_create = Chat({
+        'id': 'test_chat_id'
+    })
+
+    assert chat_second_create.storage['test'] == 'test_value'
